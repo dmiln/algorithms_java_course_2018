d3b8053e0dc7f2af56af5d605af0bbabfc38b0e9
Дмитрий <dmitilyin@gmail.com>
2018-10-10 21:08:40 +0300

    first commit

---
+++ src/practice_9_10_18/Rope.java
@@ -0,0 +1,15 @@
+package practice_9_10_18;
+
+import java.io.IOException;
+
+public class Rope extends Template {
+
+    @Override
+    public void solve() throws IOException {
+
+    }
+
+    public static void main(String[] args) {
+        new Rope().run();
+    }
+}
---
+++ src/practice_9_10_18/Template.java
@@ -0,0 +1,55 @@
+package practice_9_10_18;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public abstract class Template {
+    FastScanner in;
+    PrintWriter out;
+
+    public void solve() throws IOException {
+
+    }
+
+    public void run() {
+        try {
+            in = new FastScanner(new File("input.txt"));
+            out = new PrintWriter(System.out);
+
+            solve();
+
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastScanner(File f) {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+
+}
